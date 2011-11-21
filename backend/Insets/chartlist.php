a5cb75d00fb261e6e68a1670c0167846fe34cbe5
Pavel Kalian <pavel@kalian.cz>
2011-11-21 17:00:16 +0100

    More inset definition and saving

--- backend/Insets/chartlist.php
+++ backend/Insets/chartlist.php
@@ -7,11 +7,47 @@ $page = (int)$_GET['page']; // get the requested page
 $limit = (int)$_GET['rows']; // get how many rows we want to have into the grid
 $sidx = mysql_real_escape_string($_GET['sidx']); // get index row - i.e. user click to sort
 $sord = mysql_real_escape_string($_GET['sord']); // get the direction
+
+$fh = fopen('/var/www/virtualhosts/opencpn.xtr.cz/tmp/inset', 'w');
+fwrite($fh, var_export($_POST, TRUE));
+fwrite($fh, var_export($_REQUEST, TRUE));
+
+$wh = "";
+if($_REQUEST['_search']=='true' || $_REQUEST['search']=='true') {
+	switch ($_REQUEST['searchField']) {
+		case 'id': 
+			$wh .= " AND kap_id = ".(int)$_REQUEST['searchString']."";
+			break;
+		case 'number':
+			$wh .= " AND ".$_REQUEST['searchField']." = ".(int)$_REQUEST['searchString']."";
+			break;
+		case 'inset_id':
+			$wh .= " AND ".$_REQUEST['searchField']." = ".(int)$_REQUEST['searchString']."";
+			break;
+		case 'bsb_type':
+			$wh .= " AND ".$_REQUEST['searchField']." = '".$_REQUEST['searchString']."'";
+			break;
+		case 'title':
+			$wh .= " AND ".$_REQUEST['searchField']." = '".$_REQUEST['searchString']."'";
+			break;
+		case 'scale':
+			$wh .= " AND ".$_REQUEST['searchField']." = ".(int)$_REQUEST['searchString']."";
+			break;
+		case 'x':
+		case 'y':
+		case 'w':
+		case 'h':
+	}
+	fwrite($fh, "WH:".$wh);
+}
+
 if(!$sidx) $sidx =1;
 if(!$limit) $limit =10;
 
 mysql_select_db($db) or die("Error conecting to db.");
-$result = mysql_query("SELECT COUNT(*) AS count FROM ocpn_nga_kap WHERE active = 1 AND bsb_type != 'BASE' AND number IN (SELECT number FROM ocpn_nga_charts)");
+$SQL = 'SELECT COUNT(*) AS count FROM ocpn_nga_kap WHERE active = 1 AND bsb_type != \'BASE\' AND number IN (SELECT number FROM ocpn_nga_charts)'.$wh;
+fwrite($fh, $SQL);
+$result = mysql_query($SQL);
 $row = mysql_fetch_array($result,MYSQL_ASSOC);
 $count = $row['count'];
 
@@ -25,8 +61,9 @@ $start = $limit*$page - $limit; // do not put $limit*($page - 1)
 if ($start < 0) $start=0;
 //$SQL = "SELECT a.id, a.invdate, b.name, a.amount,a.tax,a.total,a.note FROM invheader a, clients b WHERE a.client_id=b.client_id ORDER BY $sidx $sord LIMIT $start , $limit";
 
-$SQL = "SELECT kap_id AS id, number, inset_id, bsb_type, title, scale, (SELECT MIN(x) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type='CROP') AS x, ((SELECT MAX(x) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type='CROP') - (SELECT MIN(x) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type='CROP')) AS w, ((SELECT MAX(y) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type='CROP') - (SELECT MIN(y) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type='CROP')) AS h  FROM ocpn_nga_kap k WHERE active = 1 AND bsb_type != 'BASE' AND number IN (SELECT number FROM ocpn_nga_charts) ORDER BY $sidx $sord LIMIT $start , $limit";
-//echo $SQL;
+$SQL = 'SELECT kap_id AS id, number, inset_id, bsb_type, title, scale, (SELECT MIN(x) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type=\'CROP\') AS x, ((SELECT MAX(x) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type=\'CROP\') - (SELECT MIN(x) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type=\'CROP\')) AS w, ((SELECT MAX(y) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type=\'CROP\') - (SELECT MIN(y) FROM ocpn_nga_kap_point WHERE kap_id = k.kap_id AND active = 1 AND point_type=\'CROP\')) AS h  FROM ocpn_nga_kap k WHERE active = 1 AND bsb_type != \'BASE\' AND number IN (SELECT number FROM ocpn_nga_charts)'.$wh.' ORDER BY '.$sidx.' '.$sord.' LIMIT '.$start.' , '.$limit.'';
+fwrite($fh, $SQL);
+fclose($fh);
 $result = mysql_query( $SQL ) or die("Couldn t execute query.".mysql_error());
 
 $responce->page = $page;
@@ -38,5 +75,11 @@ while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
     $responce->rows[$i]['cell']=array($row[id],$row[number],$row[inset_id],$row[bsb_type],$row[title],$row[scale],$row[x],$row[y],$row[w],$row[h]);
     $i++;
 }
+mysql_close($link);
 echo json_encode($responce);
+
+fwrite($fh, print_r($responce, TRUE));
+fclose($fh);
+
+
 ?>
---
+++ backend/Insets/save.php
@@ -0,0 +1,120 @@
+<?php
+// ### Start the session
+session_start();
+
+$_SESSION['wp-user']['id'] = 1;
+
+// ### First let's see if the user is logged-in and if not redirect to the login page
+if(!$_SESSION['wp-user']['id']) {
+	header('Location: http://opencpn.info/en/nga-charts-edit');
+	exit;
+}
+
+include("config.php");
+// connect to the database
+$link = mysql_connect($server, $usr, $pwd) or die("Connection Error: " . mysql_error());
+
+$id = (int)$_POST['id'];
+$number = (int)$_POST['number'];
+$inset_id = mysql_real_escape_string($_POST['inset_id']);
+$bsb_type = mysql_real_escape_string($_POST['bsb_type']);
+$title = mysql_real_escape_string($_POST['title']);
+$scale = (int)$_POST['scale'];
+$x = (int)$_POST['x'];
+$y = (int)$_POST['y'];
+$w = (int)$_POST['w'];
+$h = (int)$_POST['h'];
+$x1 = $x + $w;
+$y1 = $y + $h;
+
+//test the data
+if ($id == 0 || $number == 0)//The following would be confusing for added records: || $x1 == 0 || $y1 == 0 || $x1 - $x < 100 || $y1 - $y < 100)
+	die(); //Not properly defined or too small to be an inset
+
+switch($_POST['oper'])
+{
+	case 'edit':
+		$sql = 'INSERT INTO ocpn_nga_kap (number, is_main, status_id, scale, title, NU, GD ,PR, PP ,UN, SD, DTMx, DTMy, DTMdat, changed, changed_by, active, locked, bsb_type, GD_other, PR_other, UN_other, SD_other, DTMdat_other, locked_by ,comments ,noPP, noDTM, kap_generated, gpx, inset_id) SELECT number, is_main, status_id, scale, title, NU, GD ,PR, PP ,UN, SD, DTMx, DTMy, DTMdat, changed, changed_by, 0, locked, bsb_type, GD_other, PR_other, UN_other, SD_other, DTMdat_other, locked_by ,comments ,noPP, noDTM, kap_generated, gpx, inset_id FROM ocpn_nga_kap WHERE active=1 AND kap_id='.$id;
+		mysql_query($sql, $link);
+		
+		$sql = 'SELECT MIN(x) AS minx, MIN(y) AS miny FROM ocpn_nga_kap_point WHERE active=1 AND point_type=\'CROP\' AND kap_id='.$id;
+		$res = mysql_query($sql, $link);
+		if(mysql_num_rows($res) > 0)
+		{
+			$row = mysql_fetch_array($res);
+			//recalculate the existing REFs 
+			$shiftx = $x - (int)$row['minx'];
+			$shifty = $y - (int)$row['miny'];
+			$sql = 'UPDATE ocpn_nga_kap_point SET x=x-'.$shiftx.', y=y-'.$shifty.' WHERE active=1 AND kap_id='.$id.' AND point_type=\'REF\'';
+			mysql_query($sql);
+		}
+		$sql = 'UPDATE ocpn_nga_kap_point SET active = 0 WHERE point_type=\'CROP\' AND kap_id='.$id;
+		mysql_query($sql, $link);
+		$sql = 'INSERT INTO ocpn_nga_kap_point (kap_id, latitude, longitude, x, y, point_type, created_by, created, sequence, active) VALUES ('.$id.', NULL, NULL, '.$x.', '.$y.', \'CROP\', '.$_SESSION['wp-user']['id'].', CURRENT_TIMESTAMP(), 1, 1)';
+		mysql_query($sql, $link);
+		$sql = 'INSERT INTO ocpn_nga_kap_point (kap_id, latitude, longitude, x, y, point_type, created_by, created, sequence, active) VALUES ('.$id.', NULL, NULL, '.$x1.', '.$y1.', \'CROP\', '.$_SESSION['wp-user']['id'].', CURRENT_TIMESTAMP(), 2, 1)';
+		mysql_query($sql, $link);
+		break;
+	case 'add':
+		$sql = 'INSERT INTO ocpn_nga_kap (number, is_main, status_id, scale, title, NU, GD ,PR, PP ,UN, SD, DTMx, DTMy, DTMdat, changed, changed_by, active, locked, bsb_type, GD_other, PR_other, UN_other, SD_other, DTMdat_other, locked_by ,comments ,noPP, noDTM, kap_generated, gpx, inset_id) VALUES ('.$number.', 0, 16, '.$scale.', \''.$title.'\', NULL, NULL ,NULL, NULL ,NULL, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP(), '.$_SESSION['wp-user']['id'].', 1, 0, \''.$bsb_type.'\', NULL, NULL, NULL, NULL, NULL, NULL,NULL, NULL, NULL, NULL, NULL, \''.$inset_id.'\')';
+		mysql_query($sql, $link);
+		$id = mysql_insert_id($link);
+		$sql = 'INSERT INTO ocpn_nga_kap_point (kap_id, latitude, longitude, x, y, point_type, created_by, created, sequence, active) VALUES ('.$id.', NULL, NULL, '.$x.', '.$y.', \'CROP\', '.$_SESSION['wp-user']['id'].', CURRENT_TIMESTAMP(), 1, 1)';
+		mysql_query($sql, $link);
+		$sql = 'INSERT INTO ocpn_nga_kap_point (kap_id, latitude, longitude, x, y, point_type, created_by, created, sequence, active) VALUES ('.$id.', NULL, NULL, '.$x1.', '.$y1.', \'CROP\', '.$_SESSION['wp-user']['id'].', CURRENT_TIMESTAMP(), 2, 1)';
+		mysql_query($sql, $link);
+		break;
+}
+
+/*
+$fh = fopen('/var/www/virtualhosts/opencpn.xtr.cz/tmp/inset', 'w');
+fwrite($fh, var_export($_POST, TRUE));
+fwrite($fh, var_export($_REQUEST, TRUE));
+fclose($fh);
+*/
+/*
+array (
+  'number' => '14221',
+  'inset_id' => 'A',
+  'bsb_type' => 'PLAN',
+  'title' => 'Lanoraie Anchorage',
+  'scale' => '18000',
+  'x' => '2188',
+  'y' => '738',
+  'w' => '1263',
+  'h' => '950',
+  'oper' => 'edit',
+  'id' => '16100',
+)
+
+array (
+  'number' => '14800',
+  'inset_id' => 'A',
+  'bsb_type' => 'INSET',
+  'title' => 'Test',
+  'scale' => '10000',
+  'x' => '0',
+  'y' => '0',
+  'w' => '0',
+  'h' => '0',
+  'oper' => 'add',
+  'id' => '_empty',
+)
+  
+)array (
+  'number' => '111',
+  'inset_id' => 'A',
+  'bsb_type' => 'INSET',
+  'title' => 'aaa',
+  'scale' => '111',
+  'x' => '',
+  'y' => '',
+  'w' => '',
+  'h' => '',
+  'oper' => 'add',
+  'id' => '_empty',
+) 
+*/
+
+mysql_close($link);
+?>
